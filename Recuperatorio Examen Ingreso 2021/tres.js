397822169901f6de21d2bf1b3e3d89b39f016e9c
edgarluna04 <78120940+edgarluna04@users.noreply.github.com>
2021-03-10 21:20:48 -0300

    Update tres.js

--- Recuperatorio Examen Ingreso 2021/tres.js
+++ Recuperatorio Examen Ingreso 2021/tres.js
@@ -10,14 +10,14 @@ function mostrar()
 	var contadorIlluminatis=0;
 
 	var acumuladorLamparasArgenLuz;
-	flagArgentLuz==true;
+	var flagArgentLuz=true;
 
 
 	var acumuladorLamparasFelipeLamp;
-	flagFelipe==true;
+	var flagFelipe=true;
 
 	var acumuladorLamparasIllumin;
-	flagIlluminati==true;
+	var flagIlluminati=true;
 
 	var promedioArgentLuz;
 	var promedioFelipeLamp;
@@ -144,21 +144,3 @@ function mostrar()
 
 }
 
-/*
-Ferrete lamparas 2.0 El empleado del negocio ingresa los siguientes datos de cada venta, teniendo en cuenta que tenemos varias ventas:
-nombre del cliente,
-cantidad de lamparas ,
-marca (FelipeLamparas - ArgentinaLuz -Illuminatis),
-precio por unidad,
-precio total de esa compra: dato que se calcula.
-
-Se sabe que si la marca es FelipeLamparas y la cantidad de lamparas supera las 5 unidades, se 
-aplica un descuento del 10% sobre la compra 
-Si la marca es ArgentinaLuz y compra 3 o mas unidades, el descuento es del 5%. 
-Ningun otro caso aplica descuento. al terminar la carga de todas las ventas,la empresa desea saber:
-
-a) Cuanto recauda la empresa en concepto de todas las ventas realizadas.
-b) Cuanto "perdio" la empresa en concepto de descuentos.
-c) El promedio de la cantidad de lamparas vendidas de cada marca.
-d) La marca que mas ventas realizo (sin importar la cantidad de lamparas vendidas)
-*/
